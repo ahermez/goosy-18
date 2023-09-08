8151a816d11865888d6d0631637e2252bd517e08
ahermez <131738383+ahermez@users.noreply.github.com>
2023-09-08 16:44:50 -0700

    new folder created

--- models/thought.js
+++ models/thought.js
@@ -4,5 +4,9 @@ const thoughtSchema = new  mongoose.Schema({
    thoughtText: { type: String, required: true, minLength: 1, maxlength: 280 },
    createdAt: { type: Date, default: Date.now() },
    userName: { type: String, required: true },
-   reactions: [{ type: ObjectId, ref: "Reaction" }]
+   reactions: [{ type: mongoose.Types.ObjectId, ref: "Reaction" }]
 })
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
--- models/user.js
+++ models/user.js
@@ -4,10 +4,11 @@ const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
 const userSchema = new  mongoose.Schema({
     userName: { type: String, required: true, unique: true, trim: true},
     email: { type: String, required: true, unique: true, match: emailRegex },
-    thoughts: [{type: ObjectId, ref: "Thought"}],
-    friends: [{type: ObjectId, ref: "User"}]
+    thoughts: [{type: mongoose.Types.ObjectId, ref: "Thought"}],
+    friends: [{type:mongoose.Types.ObjectId, ref: "User"}]
 })
 
 const User = mongoose.model('User', userSchema);
 
 
+module.exports = User;
